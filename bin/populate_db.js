dd73ffce468a37f828fe1648ca4f9956e1e96f51
Eric Frankel <eric-frankel@cox.net>
2018-07-13 19:49:02 -0700

    added script for populating the database

---
+++ bin/populate_db.js
@@ -0,0 +1,109 @@
+const async = require('async');
+const mongoose = require('mongoose');
+require(process.cwd() + '/lib/connection');
+const Employee = mongoose.model('Employee');
+const Team = mongoose.model('Team');
+
+var data = {
+  employees: [
+    {
+      id: '1000003',
+      name: {
+        first: 'Colin',
+        last: 'Ihrig'
+      },
+      image: 'images/employees/1000003.png',
+      address: {
+        lines: ['11 Wall Street'],
+        city: 'New York',
+        state: 'NY',
+        zip: 10118
+      }
+    },  {
+      id: '1000021',
+      name: {
+        first: 'Adam',
+        last: 'Bretz'
+      },
+      address: {
+        lines: ['46 18th St', 'St. 210'],
+        city: 'Pittsburgh',
+        state: 'PA',
+        zip: 15222
+      }
+    },
+    {
+      id: '1000022',
+      name: {
+        first: 'Matt',
+        last: 'Liegey'
+      },
+      address: {
+        lines: ['2 S Market Square', '(Market Square)'],
+        city: 'Pittsburgh',
+        state: 'PA',
+        zip: 15222
+      }
+    },
+    {
+      id: '1000025',
+      name: {
+        first: 'Aleksey',
+        last: 'Smolenchuk'
+      },
+      image: 'images/employees/1000025.png' /* invalid image */,
+      address: {
+        lines: ['3803 Forbes Ave'],
+        city: 'Pittsburgh',
+        state: 'PA',
+        zip: 15213
+      }
+    },
+    {
+      id: '1000030',
+      name: {
+        first: 'Sarah',
+        last: 'Gay'
+      },
+      address: {
+        lines: ['8651 University Blvd'],
+        city: 'Pittsburgh',
+        state: 'PA',
+        zip: 15108
+      }
+    },
+    {
+      id: '1000031',
+      name: {
+        first: 'Dave',
+        last: 'Beshero'
+      },
+      address: {
+        lines: ['1539 Washington Rd'],
+        city: 'Mt Lebanon',
+        state: 'PA',
+        zip: 15228
+      }
+    }
+  ],
+  teams: [
+    {
+      name: 'Software and Services Group'
+    },
+    {
+      name: 'Project Development'
+    }
+  ]
+}
+
+var deleteEmployees = function(callback) {
+  console.info('Deleting employees');
+  Employee.remove({}, function(error, response) {
+    if (error) {
+      console.log('Error deleting employees: ' + error);
+    }
+
+    console.info('Done deleting employees');
+    callback();
+  })
+}
